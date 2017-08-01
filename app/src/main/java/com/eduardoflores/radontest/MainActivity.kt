daebfa5337d3dfd12ba5c1ba2b33ca8b994aeacf
Eduardo Flores <eflores@ancestry.com>
2017-07-31 21:12:27 -0600

    changed class variables to lateinit

--- app/src/main/java/com/eduardoflores/radontest/MainActivity.kt
+++ app/src/main/java/com/eduardoflores/radontest/MainActivity.kt
@@ -7,6 +7,7 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 /**
  * https://console.firebase.google.com/project/radontest-1005/overview
@@ -14,26 +15,34 @@ import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     val TAG = "MainActivity"
+    lateinit var mFirebaseAnalytics : FirebaseAnalytics
+    lateinit var mAuth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupAnalytics()
+        if (getCurrentFirebaseUser() == null) {
+            signUpNewUser(mAuth)
+        }
+    }
+
+    private fun getCurrentFirebaseUser(): FirebaseUser? {
+        // authentication
+        mAuth = FirebaseAuth.getInstance()
+        val currentUser = mAuth.currentUser
+        Log.d(TAG, "Current user: " + currentUser)
+        return currentUser
+    }
+
+    private fun setupAnalytics() {
         // analytics
-        val mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
         val bundle = Bundle()
         bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "test")
         bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "some_name")
         mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
-
-        // authentication
-        val mAuth = FirebaseAuth.getInstance()
-        val currentUser = mAuth.currentUser
-        Log.d(TAG, "Current user: " + currentUser)
-
-        if (currentUser == null) {
-            signUpNewUser(mAuth)
-        }
     }
 
     fun signUpNewUser(auth: FirebaseAuth) {
